8ff45af8b1b34b439299e75aed88d7302cfc9a09
Cagri Yildiz <cgr.yldz.1212@gmail.com>
2023-02-11 22:59:30 +0100

    Add generic blend fucntionality (#19)

--- AnimationSystem/Animation/SkeletonPose.hpp
+++ AnimationSystem/Animation/SkeletonPose.hpp
@@ -20,6 +20,7 @@ namespace AnimationSystem
         SkeletonPose(const aiScene* pScene): _scene(*pScene), _pSkeleton(std::make_shared<Skeleton>()){}
         SkeletonPose(std::shared_ptr<Skeleton> pSkeleton);
         void updateGlobalPose(std::string name, simd::float4x4 pose);
+        void updateGlobalPose(int id, simd::float4x4 pose);
         void addJointName(std::string name, size_t id) noexcept {_jointNameToIdMap[name] = id;}
         [[nodiscard]] int getJointId(const std::string &name)
         {
@@ -35,6 +36,13 @@ namespace AnimationSystem
         std::shared_ptr<Skeleton> getSkeleton(){return _pSkeleton;}
         void compFinalTransformations();
         std::vector<simd::float4x4> getFinalTransformations() const noexcept {return _finalTransformations;}
+        simd::float4x4 getGlobalPose(int id){ return _globalPoses[id]; };
+        simd::float3 getTranslation(int id);
+        simd::float3 getScale(int id);
+        simd::quatf getRotation(int id);
+        void setTranslation(int id, simd::float3 t);
+        void setScale(int id, float s);
+        void setRotation(int id, simd::quatf r);
         
     private: 
         std::shared_ptr<Skeleton> _pSkeleton;
